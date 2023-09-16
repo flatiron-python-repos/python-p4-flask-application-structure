3e6a2891301de2ad313522c97bc86754c4dc324f
George-Okumu <gokumu12@gmail.com>
2023-09-16 04:00:56 +0300

    first routes in flask

--- server/app.py
+++ server/app.py
@@ -3,3 +3,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to me</h1>'
+
+@app.route('/<int:id>')
+def user_id(id):
+    return f'user id passed in parameter is: {id}'
+
+
+if __name__ == '__main__':
+    app.run(port=8001)
